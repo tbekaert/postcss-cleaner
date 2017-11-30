4dcf6039d113c168c9542f971c68531808f1b38b
Thomas <tbekaert@siliconsalad.com>
2017-11-30 16:45:52 +0100

    Add some tests

--- index.js
+++ index.js
@@ -4,105 +4,114 @@ var postcss = require('postcss');
 var glob = require('glob');
 var fs = require('fs');
 
-module.exports = postcss.plugin('postcss-css-cleaner', function(opts) {
-  opts = opts || {};
+module.exports = postcss.plugin('postcss-css-cleaner', function (opts) {
+    opts = opts || {};
 
-  let log = function(type, args) {
-    if (opts.log) {
-      if (
-        (type == 'sourcesList' && opts.log.sourcesList) ||
-        (type == 'removedRules' && opts.log.removedRules) ||
-        (type == 'ignoredRules' && opts.log.ignoredRules)
-      ) {
-        console.log(...args);
-      }
-    }
-  };
-
-  let flatten = arr =>
-    arr.reduce(
-      (a, b) => (Array.isArray(b) ? a.concat(flatten(b)) : a.concat(b)),
-      []
-    );
-
-  let getFilesContent = function() {
-    let patterns = flatten(arguments[0]);
-
-    log('sourcesList', ['=== Source files ===']);
-
-    return flatten(
-      patterns.map(pattern => {
-        return glob.sync(pattern).map(file => {
-          log('sourcesList', [file]);
-          return new String(fs.readFileSync(file));
-        });
-      })
-    ).join('\n');
-  };
-
-  if (Array.isArray(opts.sources)) {
-    opts.raw = getFilesContent(opts.sources);
-  } else if (!opts.raw) {
-    // error because sources is not an array and raw is not defined
-  }
-  opts.ignore = [/[\d]*%/].concat(opts.ignore ? opts.ignore : []);
-
-  return function(root, result) {
-    if (!opts.raw) {
-      result.warn(`Array of sources or raw source is necessary to run this plugin. See doc.`)
-      return;
-    }
-    root.walk(rule => {
-      if (rule.type == 'rule') {
-        let selectors = rule.selector.replace(/[\n]/, '').split(',');
-        let testSelectors = selectors.map(s => {
-          return s
-            .replace(/\.|#|\=|(\[[^=]*=)|\]/g, ' ')
-            .replace(/\[/g, ' ')
-            .replace(/>|~|\+|\'|"|\n|\t|\*|:not\([^\)]*\)|(::?[^\s:]*)/g, '')
-            .split(' ')
-            .filter(sel => sel !== '');
-        });
-
-        testSelectors.map((sel, i) => {
-          let isIgnored = sel
-            .map(s => {
-              return opts.ignore
-                .map(r => {
-                  return typeof r === 'string'
-                    ? r.replace(/\.|#/g, '') === s
-                    : r.test(s);
-                })
-                .reduce((r, b) => (r ? r : b), false);
-            })
-            .reduce((s, b) => (s ? s : b), false);
-
-          if (!isIgnored) {
-            let isFound = sel
-              .map(s => opts.raw.indexOf(s) >= 0 )
-              .reduce((s, b) => (!s ? s : b), true);
-
-            if (!isFound) {
-              if(selectors[i]){
-                log('removedRules', [
-                  `Remove selector '${selectors[i]}' line ${
-                    rule.source.start.line
-                  }`
-                ]);
-              }
-              selectors.splice(i, 1);
+    let log = function (type, args) {
+        if (opts.log) {
+            if (
+                type === 'sourcesList' && opts.log.sourcesList ||
+                type === 'removedRules' && opts.log.removedRules ||
+                type === 'ignoredRules' && opts.log.ignoredRules
+            ) {
+                console.log(...args);
             }
-          } else {
-            log('ignoredRules', [
-              `Ignore selector '${selectors[i]}' line ${rule.source.start.line}`
-            ]);
-          }
+        }
+    };
+
+    let flatten = arr =>
+        arr.reduce(
+            (a, b) => Array.isArray(b) ? a.concat(flatten(b)) : a.concat(b),
+            []
+        );
+
+    let getFilesContent = function () {
+        let patterns = flatten(arguments[0]);
+
+        log('sourcesList', ['=== Source files ===']);
+
+        return flatten(
+            patterns.map(pattern => {
+                return glob.sync(pattern).map(file => {
+                    log('sourcesList', [file]);
+                    let content = fs.readFileSync(file);
+                    return content;
+                });
+            })
+        ).join('\n');
+    };
+
+    if (Array.isArray(opts.sources)) {
+        opts.raw = getFilesContent(opts.sources);
+    } else if (!opts.raw) {
+        // error because sources is not an array and raw is not defined
+    }
+    opts.ignore = [/[\d]*%/].concat(opts.ignore ? opts.ignore : []);
+
+    return function (root, result) {
+        if (!opts.raw) {
+            result.warn(
+                'Array of sources or raw source is ' +
+                'necessary to run this plugin. See doc.'
+            );
+            return;
+        }
+        root.walk(rule => {
+            if (rule.type === 'rule') {
+                let selectors = rule.selector.replace(/[\n]/, '').split(',');
+                let testSelectors = selectors.map(s => {
+                    return s
+                        .replace(/\.|#|\=|(\[[^=]*=)|\]/g, ' ')
+                        .replace(/\[/g, ' ')
+                        .replace(
+                            />|~|\+|\'|"|\n|\t|\*|:not\([^\)]*\)|(::?[^\s:]*)/g,
+                            ''
+                        )
+                        .split(' ')
+                        .filter(sel => sel !== '');
+                });
+
+                testSelectors.map((sel, i) => {
+                    let isIgnored = sel
+                        .map(s => {
+                            return opts.ignore
+                                .map(r => {
+                                    return typeof r === 'string' ?
+                                        r.replace(/\.|#/g, '') === s :
+                                        r.test(s);
+                                })
+                                .reduce((r, b) => r ? r : b, false);
+                        })
+                        .reduce((s, b) => s ? s : b, false);
+
+                    if (!isIgnored) {
+                        let isFound = sel
+                            .map(s => opts.raw.indexOf(s) >= 0)
+                            .reduce((s, b) => !s ? s : b, true);
+
+                        if (!isFound) {
+                            if (selectors[i]) {
+                                log('removedRules', [
+                                    `Remove selector '${selectors[i]}' line ${
+                                        rule.source.start.line
+                                    }`
+                                ]);
+                            }
+                            selectors.splice(i, 1);
+                        }
+                    } else {
+                        log('ignoredRules', [
+                            `Ignore selector '${selectors[i]}' line ${
+                                rule.source.start.line
+                            }`
+                        ]);
+                    }
+                });
+
+                rule.selector = selectors.join(', ');
+
+                if (rule.selector === '') rule.remove();
+            }
         });
-
-        rule.selector = selectors.join(', ');
-
-        if (rule.selector == '') rule.remove();
-      }
-    });
-  };
+    };
 });
--- index.test.js
+++ index.test.js
@@ -3,17 +3,77 @@ var postcss = require('postcss');
 var plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
+    return postcss([plugin(opts)])
+        .process(input)
         .then(result => {
             expect(result.css).toEqual(output);
             expect(result.warnings().length).toBe(0);
         });
 }
 
-/* Write tests here
+function runError(input, output, opts) {
+    return postcss([plugin(opts)])
+        .process(input)
+        .then(result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBeGreaterThan(0);
+        });
+}
 
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+/**
+ * Options
+ */
+
+it('return same CSS and a warning if no sources provided', () => {
+    return runError('a{ }', 'a{ }', {});
 });
 
-*/
+it('return same CSS and a warning if opts.sources is not an array', () => {
+    return runError('a{ }', 'a{ }', {
+        sources: 'lorem'
+    });
+});
+
+/**
+ * Removal
+ */
+
+it('remove unused css in html sources', () => {
+    return run('.foo{ } .bar{ } .baz{ }', '.foo{ } .baz{ }', {
+        raw: '<div class="foo"><p class="baz">Lorem</p></div>'
+    });
+});
+
+it('remove unused css in slim sources', () => {
+    return run('.foo{ } .bar{ } .baz{ }', '.foo{ } .bar{ }', {
+        raw: `
+            div.foo
+              p class="bar"
+                | Lorem
+        `
+    });
+});
+
+it('remove unused css in js sources', () => {
+    return run('.foo{ } .bar{ } .baz{ }', '.bar{ }', {
+        raw: 'document.querySelectorAll(".bar")'
+    });
+});
+
+/**
+ * Ignorance
+ */
+
+it('ignore rule based on string', () => {
+    return run('.foo{ } .bar{ } .baz{ }', '.bar{ }', {
+        raw: '<div class="a"><p class="b">Lorem</p></div>',
+        ignore: ['.bar']
+    });
+});
+
+it('ignore rule based on regex', () => {
+    return run('.foo{ } .bar{ } .baz{ }', '.bar{ } .baz{ }', {
+        raw: '<div class="a"><p class="b">Lorem</p></div>',
+        ignore: [/ba/]
+    });
+});
